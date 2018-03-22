fceb8e52ed17f3738823a6d50c2a5948622ff616
Mincong HUANG <mincong.h@gmail.com>
2018-03-22 23:04:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# gwt-safecss [![Build Status][travis-img]][travis]
+
+GWT SafeCss: facilities for avoiding XSS attacks.
+
+[travis]: https://travis-ci.org/mincong-h/gwt-safecss
+[travis-img]: https://travis-ci.org/mincong-h/gwt-safecss.svg?branch=master
---
+++ src/test/java/org/gwtproject/safecss/SafeCssJreSuite.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.gwtproject.safecss;
+
+import org.gwtproject.safecss.shared.SafeStylesBuilderTest;
+import org.gwtproject.safecss.shared.SafeStylesHostedModeUtilsTest;
+import org.gwtproject.safecss.shared.SafeStylesStringTest;
+import org.gwtproject.safecss.shared.SafeStylesUtilsTest;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+import junit.framework.Test;
+
+/**
+ * Test suite for SafeCss GWTTestCases.
+ */
+public class SafeCssJreSuite {
+  public static Test suite() {
+    GWTTestSuite suite = new GWTTestSuite("Test suite for safe css tests that require the JRE");
+
+    suite.addTestSuite(SafeStylesBuilderTest.class);
+    suite.addTestSuite(SafeStylesHostedModeUtilsTest.class);
+    suite.addTestSuite(SafeStylesStringTest.class);
+    suite.addTestSuite(SafeStylesUtilsTest.class);
+
+    return suite;
+  }
+
+  private SafeCssJreSuite() {
+  }
+}
